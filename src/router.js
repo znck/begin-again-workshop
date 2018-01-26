6185df42e892827cf0a0035eaa2cee7298e23b20
Rahul Kadyan <hi@znck.me>
2018-01-27 01:41:30 +0530

    Create `/notes` page
    - Create `Home.vue` file in `src/pages/`
    
    - Register `/notes` route with `Home.vue`
      Add redirect rule for `/` route.

--- src/router.js
+++ src/router.js
@@ -4,7 +4,9 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 const routes = [
+  { name: 'home', path: '/notes', component: () => import('./pages/Home.vue') },
   { name: 'create', path: '/notes/create', component: () => import('./pages/Create.vue') },
+  { path: '/', redirect: '/notes' }
 ]
 
 export default new Router({ mode: 'history', routes })
