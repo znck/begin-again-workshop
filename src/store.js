a4e51f766f982d6fe7925b3e55955ac64602d9fe
Rahul Kadyan <hi@znck.me>
2018-01-27 04:17:26 +0530

    Add note create and update functions
    - Add `create` and `update` actions
    
    - Add `CREATE` and `UPDATE` mutations
    
    - Add computed getters
      Get note by ID
      Get notes sorted by last updated at

--- src/store.js
+++ src/store.js
@@ -4,7 +4,11 @@ import { desc, uuid } from './utils'
 
 Vue.use(Vuex)
 
+const CREATE = 'CREATE'
+const UPDATE = 'UPDATE'
+
 const getters = {
+  noteById: ({ notes }) => id => notes.find(it => it.id === id),
   notesSortedByLastUpdated: ({ notes }) => notes.slice().sort((a, b) => desc(a.updatedAt, b.updatedAt))
 }
 
@@ -12,9 +16,51 @@ const state = () => ({
   notes: []
 })
 
-const mutations = {}
+const mutations = {
+  [CREATE]: (state, note) => { state.notes.push(note) },
+  [UPDATE]: (state, [old, note]) => { state.notes.splice(state.notes.indexOf(old), 1, note) }
+}
 
-const actions = {}
+const actions = {
+  async save({ dispatch }, payload) {
+    if (payload.id) return dispatch('update', payload)
+    
+    return dispatch('create', payload)
+  },
+
+  async create({ commit }, { title, body }) {
+    const timestamp = new Date()
+    const note = {
+      id: uuid(),
+      title,
+      body,
+      createdAt: timestamp,
+      updatedAt: timestamp
+    }
+
+    commit(CREATE, note)
+
+    return note
+  },
+  
+  async update({ commit, getters }, { id, title, body }) {
+    const note = getters.noteById(id)
+
+    if (!note) throw Error('Cannot update non-existing note.')
+
+    const payload = {
+      ...note,
+      id,
+      title,
+      body,
+      updatedAt: new Date()
+    }
+
+    commit(UPDATE, [note, payload])
+
+    return payload
+  }
+}
 
 const store = new Store({
   getters,
