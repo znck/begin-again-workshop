2253aa9f1d203b9b0b6cafe51b57781564418c5c
Rahul Kadyan <hi@znck.me>
2018-01-27 01:23:03 +0530

    Add support for client side routing
    - Install vue-router
      ``` bash
      npm add vue-router
      ```
    
    - Create `router.js` file in `src/` directory
      Register `vue-router` plugin
      Export a history router
    
    - Use router in the application
      Add `router` to Vue constructor options
    
    - Use `<router-view>` component in `App.vue` to render routes
    
    - Remove `App.spec.js`

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,12 @@
 <template>
   <div class="app">
-    <Editor v-model="content" />
-    <div class="sep" />
-    <Markdown :content="content" />
+    <router-view></router-view>
   </div>
 </template>
 
-
 <script>
-import Editor from './components/Editor.vue'
-import Markdown from './components/Markdown.vue'
 export default {
-  name: 'MarkdownNotebook',
-  components: { Editor, Markdown },
-  data: () => ({
-    content: ''
-  })
+  name: 'MarkdownNotebook'
 }
 </script>
 
@@ -26,23 +17,8 @@ html, body {
   height: 100%;
   font-size: 16px;
 }
-</style>
 
-
-<style scoped>
-.app {
-  display: flex;
-  flex-direction: row;
-  min-height: 100%;
-}
-
-.app > * {
-  flex: 1;
-}
-
-.sep {
-  flex: 0;
-  border: 1px solid #ccc;
-  margin: 0 15px;
-}
+ .app {
+   height: 100%;
+ }
 </style>
